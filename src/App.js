e8a8aeb9b2245c25fee709a72fc34352f3668a4a
thejeffrey75 <116521461+thejeffrey75@users.noreply.github.com>
2023-05-25 03:03:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import CalendarApp from "./components/Calendar";
+import ToDoForm from "./components/ToDoForm";
+import logo from './assets/logo-transparent-png.png';
+import "./App.css";
+
+// import ToDoList from "./components/ToDoList";
+
+function App() {
+  return (
+ 
+   
+
+    < div >
+<img className="logo" src={logo} alt="Logo"></img>
+         <ToDoForm/>
+       
+         <div>
+         
+         </div>
+         <CalendarApp />
+    
+    </div>
+  
+  );
+}
+
+export default App;
 
